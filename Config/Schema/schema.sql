a183abe9b292df3a67b2d6e27e4f75c42069a9cd
Diosdado D. Campo <csicebu@gmail.com>
2013-01-11 17:43:43 +0800

    oopps..

--- Config/Schema/schema.sql
+++ Config/Schema/schema.sql
@@ -1,9 +1,9 @@
 
 
-DROP TABLE IF EXISTS `cake_plugin_tester`.`accountable`;
+DROP TABLE IF EXISTS `accountable`;
 
 
-CREATE TABLE `cake_plugin_tester`.`accountable` (
+CREATE TABLE `accountable` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`resource` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
 	`actor_id` int(11) NOT NULL,
